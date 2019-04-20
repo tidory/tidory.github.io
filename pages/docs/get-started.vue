523214eb929946e402323245c3ebd6c40b0e49af
Mansu Jeong <pronist@naver.com>
2019-04-21 02:25:06 +0900

    add: npx

--- pages/docs/get-started.vue
+++ pages/docs/get-started.vue
@@ -7,29 +7,22 @@ app-page(:active="$store.state.menu.getStarted.page")
     article.paragraph
       h2(id="시작하기 전에") 시작하기 전에
       p 티도리 프레임워크는 #[b HTML, CSS, Javascript] 에 대한 기본적인 지식이 있다고 가정합니다. 따라서 해당 지식이 없다면 프레임워크를 배우기는 어려울 수 있습니다. 또한 템플릿 엔진으로 #[b 퍼그(pug)]를 사용합니다. 퍼그는 HTML의 코드 길이를 줄여주는 것은 물론, 그 외 부가적으로 다양한 기능을 제공하는 템플릿 엔진입니다. 따라서 기본 지식이외에 #[b 퍼그]를 추가적으로 알고있어야 합니다.
-    article.paragraph  
+    article.paragraph
       h2(id="티도리") 티도리(Tidory)
-      p #[b 티도리(Tidory)]는 티도리 프레임워크와 상호작용하기 위한 #[b CLI(Command Line Interface)] 환경에서 실행하는 #[b NPM(Node Package Manager)] 패키지입니다. 다음과 같은 명령을 통해 티도리를 설치해야합니다.
+      p #[b 티도리(Tidory)]는 티도리 프레임워크와 상호작용하기 위한 #[b CLI(Command Line Interface)] 환경에서 실행하는 #[b NPM(Node Package Manager)] 패키지입니다. 다음과 같은 명령을 사용하여 템플릿을 받아온 뒤, 프로젝트 구동을 위한 #[b NPM] 패키지들을 설치하십시오.
       pre
         code.lang-bash
-          | # Install TIDORY module
-          | npm install -g tidory
+          | # Download TIDORY project
+          | npx tidory new MyApp
+          | # Move into project folder
+          | cd MyApp
+          | # Install NPM Packages
+          | npm install
       p
         blockquote.blockquote-type-2
           | * CLI(Command Line Interface): 명령줄 인터페이스를 의미합니다. 이것을 지원하는 환경에는 윈도우의 cmd, powershell 그리고 리눅스의 terminal 이 있습니다.
           br
           | * NPM(Node Package Manager): 자바스크립트 런타임 환경인 node.js 용 패키지 관리자입니다. 프론트엔드과 백엔드 구분없이 다양한 패키지를 이용할 수 있습니다.
-    article.paragraph
-      h2(id="티도리 프로젝트 설치") 티도리 프로젝트 설치
-      p #[b 티도리]를 설치했다면 프로젝트 템플릿을 #[b github] 저장소로부터 받아올 수 있습니다. 다음과 같은 명령을 사용하여 템플릿을 받아온 뒤, 프로젝트 구동을 위한 #[b NPM] 패키지들을 설치하십시오.
-      pre
-        code.lang-bash
-          | # Download TIDORY project
-          | tidory new MyApp
-          | # Move into project folder
-          | cd MyApp
-          | # Install NPM Packages
-          | npm install
       p 
         blockquote.blockquote-type-2 기본 프로젝트 템플릿은 #[b 깃허브 저장소] #[a(href="https://github.com/pronist/tidory-starter-template") https://github.com/pronist/tidory-starter-template] 에서 찾을 수 있습니다.
     article.paragraph  
