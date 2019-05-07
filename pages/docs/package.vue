907d29d79012d2df131028d2aaef952a68a58588
Mansu Jeong <pronist@naver.com>
2019-05-07 15:17:13 +0900

    update: package

--- pages/docs/package.vue
+++ pages/docs/package.vue
@@ -18,12 +18,12 @@ app-page(:active="$store.state.menu.advanced.package.page")
       p #[b 티도리 패키지]의 기본 폴더구조이며 필수적인 것은 아닙니다. 하지만 프로젝트 템플릿에서 기본적으로 포함하는 템플릿은 #[b index.pug] 입니다. 기본 패키지 예제는 #[a(href="https://github.com/tidory/default-header" target="_blank") default-header] 예제를 참고해주세요.
     article.paragraph
       h2(id="package-development") 패키지 개발
-      p 티도리 패키지는 #[b 취급]을 #[b NPM 패키지]로 취급하기 때문에 #[b package.json] 이 필수적입니다. #[b 티도리 패키지]는 다른 패키지와는 달리 테스트하기 어려운 점이 있는데, 우리가 기존에 사용하던 #[b tidory-cli] 를 사용하면 #[b 개발 서버, 프리뷰 서버]를 똑같이 이용할 수 있습니다. 티도리에 등록된 #[a(href="https://github.com/tidory/package" target="_blank") package] 레포지토리를 사용하면 패키지 개발을 조금 더 쉽게할 수 있습니다.
+      p 티도리 패키지는 #[b 취급]을 #[b NPM 패키지]로 취급하기 때문에 #[b package.json] 이 필수적입니다. #[b 티도리 패키지]는 다른 패키지와는 달리 테스트하기 어려운 점이 있는데, 우리가 기존에 사용하던 #[b tidory-cli] 를 사용하면 #[b 개발 서버, 프리뷰 서버]를 똑같이 이용할 수 있습니다. 티도리에 등록된 패키지인 #[b default-header, default-sidebar, example] 레포지토리를 사용하면 패키지 개발을 조금 더 쉽게할 수 있습니다.
       h3(id="package.json") package.json
-      p 티도리 패키지는 #[b NPM 패키지]로 #[b 취급]하고 패키지 매니저 또한 #[b npm] 을 사용합니다. 하지만 본질적으로 NPM 패키지라고 보기에는 무리가 있습니다. 따라서 패키지에는 이름에 제약을 걸어둡니다. 티도리 패키지는 #[b @tidory] 스코프 아래에 두는 것을 원칙으로 합니다. 또한 NPM 레지스트리에 #[b 공표(Publish)] 하는 일이 없도록 하십시오. 레지스트리는 #[b github] 를 사용하세요.
+      p 패키지 매니저는 #[b npm] 을 사용하지만 본질적으로 NPM 패키지라고 보기에는 무리가 있습니다. 따라서 패키지에는 이름에 제약을 걸어둡니다. 티도리 패키지는 #[b @tidory] 스코프 아래에 두는 것을 원칙으로 합니다. 또한 NPM 레지스트리에 #[b 공표(Publish)] 하는 일이 없도록 하십시오. 레지스트리는 #[b github] 를 사용하세요.
       pre(data-label="package.json")
         code.lang-json {{ pkg }}
-      p #[b 패키지 개발 전용]으로 제공된 #[b 레포지토리]에는 #[b env, tidory.config.js] 등 개발을 위한 파일이 포함되어 있는데, #[b 티도리 프로젝트 템플릿]에서 포함할 때는 의미가 없으므로 #[b files] 배열에 포함시킬 필요가 없습니다. 하지만 #[b 컴포넌트] 또는 #[b 템플릿]이 위치하는 디렉토리가 있다면 그것을 추가시켜야 합니다.
+      p #[b 패키지]로 제공된 #[b 레포지토리]에는 #[b env, tidory.config.js] 등 개발을 위한 파일이 포함되어 있는데, #[b 티도리 프로젝트 템플릿]에서 포함할 때는 의미가 없으므로 #[b files] 배열에 포함시킬 필요가 없습니다. 하지만 #[b 컴포넌트] 또는 #[b 템플릿]이 위치하는 디렉토리가 있다면 그것을 추가시켜야 합니다.
       h3(id="뷰 & 리액트 컴포넌트") 뷰 & 리액트 컴포넌트
       p 티도리 프레임워크는 #[b 뷰 컴포넌트]와 #[b 리액트 컴포넌트] 해석이 가능합니다. 티도리 패키지 내부에서 컴포넌트를 당연히 포함시킬 수 있습니다. 기존에 #[b app.js] 에서 작성했던 것을 그대로 #[b index.js] 에 작성하면 됩니다. 컴포넌트가 포함된 패키지는 #[a(href="https://github.com/tidory/default-sidebar" target="_blank") default-sidebar] 예제를 참고해주세요.
     article.paragraph
