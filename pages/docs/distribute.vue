378abaf04fab0a9f185a5959abe3ae068f794e7f
Mansu Jeong <pronist@naver.com>
2019-11-16 04:02:47 +0900

    update: distribute: text

--- pages/docs/distribute.vue
+++ pages/docs/distribute.vue
@@ -66,7 +66,7 @@ app-page(:active="$store.state.menu.basic.distribute.page")
       p #[b 깃허브(Github)]와 같은 코드 저장소와 스킨 코드를 연동하려면 어떻게 하면 좋을까요? 티스토리 스킨은 #[b FTP(File Transfer Protocol)]을 지원하는 것도 아니며 그렇다고 공식적으로 코드 저장소와의 코드 공유를 지원하지도 않습니다.
       p 하지만, 한 가지 해결책이 있습니다. 티도리 프레임워크를 통해 빌드된 소스코드를 코드 저장소에 배포하면서 동시에 티스토리 스킨에 코드를 적용시킨다면 가능합니다. #[b 빌드(Build) - 코드 저장소 배포(Push) - 스킨 적용(Deploy)]의 과정을 통해 코드 저장소와 스킨 코드를 동일하게 구성할 수 있습니다.
       h3(id="push-dir") push-dir
-      p #[b push-dir]은 코드 저장소에 특정 디렉토리를 특정 브랜치에 푸쉬할 수 있도록 해주는 패키지입니다. 해당 패키지가 있으면 스킨을 빌드하면 나오는 #[b dist] 디렉토리만을 독립적으로 다른 브랜치에 올릴 수 있습니다.
+      p #[b push-dir] 은 코드 저장소에 특정 디렉토리를 특정 브랜치에 푸쉬할 수 있도록 해주는 패키지입니다. 해당 패키지가 있으면 스킨을 빌드하면 나오는 #[b dist] 디렉토리만을 독립적으로 다른 브랜치에 올릴 수 있습니다.
       pre
         code.lang-bash
           | npm install push-dir
@@ -75,15 +75,15 @@ app-page(:active="$store.state.menu.basic.distribute.page")
       pre
         code.lang-javascript
           | {{ deploy }}
-      p #[b npm run deploy:github] 명령을 따로 만들고, 배포 순서에 따라 빌드를 하고, #[b push-dir] 을 먼저 하는 것으로 코드 저장소에 푸쉬한 다음, 티스토리 스킨에 적용시키도록 바꿔주면 코드 저장소와 스킨 코드를 동일하게 만들 수 있습니다.
+      p #[b npm run deploy:github] 명령을 따로 만들고, 배포 순서에 따라 빌드를 하고, #[b push-dir] 을 먼저 하는 것으로 코드 저장소에 푸쉬한 다음, 스킨에 적용시키도록 바꿔주면 코드 저장소와 스킨 코드를 동일하게 만들 수 있습니다. 티스토리에서 제공하는 #[b 스킨 편집]은 없다고 여기십시오.
       pre
         code.lang-javascript
           | {{ pushDir }}
       p 
         blockquote.blockquote-type-2 위와 같이 명령어를 구성하면 #[b 빌드(Build) - 코드 저장소 배포(Push) - 스킨 적용(Deploy)]의 순서로 배포를 자동화할 수 있습니다. 코드 저장소에 #[b tistory-skin] 브랜치가 별도로 생성되어 개발코드가 아닌, 오직 스킨의 코드만 푸쉬됩니다. #[b 테스트용 스킨 프로젝트]인 #[a(href="https://github.com/pronist/tistory-skin") https://github.com/pronist/tistory-skin] 을 참고하십시오.
       h2(id="tistory-skin-api") 티스토리 스킨 API
-      p 티도리 프레임워크가 아닌 #[b 다른 프로젝트]에 자신의 스킨 프로젝트를 직접 연동할 수 있도록 만들어 보고 싶을 수도 있습니다. 티도리 프레임워크에서 #[b 티스토리 서버와 통신]하는 부분(프리뷰, 배포, 저장)에는 필자가 작성한 특별한 인터페이스가 사용됩니다. 
-      p #[a(href="https://github.com/tidory/tistory-skin") 티스토리 스킨 API] 가 있다면 연동이 완전히 불가능한 것은 아닙니다. 해당 API 는 티스토리 스킨에 대한 조작을 할 수 있습니다. #[b 티스토리 스킨 편집]에 있는 대부분의 기능(파일 및 스킨 추가, 삭제, 프리뷰, 스킨코드 변경 등)을 프로그래밍이 가능한 형태로 제공합니다.
+      p 티도리 프레임워크가 아닌 #[b 다른 프로젝트]에 자신의 스킨 프로젝트를 직접 연동할 수 있도록 만들어 보고 싶을 수도 있습니다. 티도리 프레임워크에서 #[b 티스토리 서버와 통신]하는 부분(프리뷰, 배포, 저장)에는 필자가 작성한 #[b API(Application Programming Interface)]가 사용됩니다. 
+      p #[a(href="https://github.com/tidory/tistory-skin") (Unofficial) Tistory Skin] 을 사용하면, #[b 티스토리 스킨 저장소]를 조작을 할 수 있습니다. #[b 스킨 편집]에 있는 대부분의 기능(파일 및 스킨 추가, 삭제, 프리뷰, 스킨코드 변경 등)을 프로그래밍이 가능한 형태로 제공합니다.
       pre
         code.lang-javascript
           | {{ deployer }}
