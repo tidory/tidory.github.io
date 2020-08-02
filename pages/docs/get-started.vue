0dcd80406567e986b48a0586492ddd11f3f10fc0
pronist <pronist@naver.com>
2020-08-03 00:29:01 +0900

    update: get-started.vue

--- pages/docs/get-started.vue
+++ pages/docs/get-started.vue
@@ -9,14 +9,12 @@ app-page(:active="$store.state.menu.basic.getStarted.page")
       p #[b Tidory CLI] 는 티도리 프레임워크와 상호작용하기 위한 #[b CLI(Command Line Interface)] 환경에서 실행하는 #[b NPM(Node Package Manager)] 패키지입니다. 다음과 같은 명령을 사용하여 템플릿을 받아온 뒤, 프로젝트 구동을 위한 #[b NPM] 패키지들을 설치하십시오.
       pre
         code.bash
-          | # Tidory 7.2 ~
+          | # Download tidory project template
           | npx create-tidory-app my-app
           | # Move into the folder
           | cd my-app
           | # Install NPM Packages
           | npm install
-      p
-        blockquote.blockquote-type-2 #[b Tidory 7.2] 보다 하위버전을 사용한다면, #[b tidory new my-app] 명령어로 프로젝트를 다운받을 수 있습니다. 기본 프로젝트 템플릿은 #[a(href="https://github.com/tidory/tidory") 깃허브]에서 찾을 수 있습니다.
     article.paragraph
       h2 라이브(Live) 서버
       p 데모로 작성되어있는 앱을 어떻게 실행시키면 될까요? 간단하게 알아보도록 하겠습니다. #[b tidory start] 명령을 사용하면 #[b http://localhost:8080] 주소로 #[b webpack-dev-server] 를 실행시킬 수 있습니다. 프리뷰 서버는 #[b tidory preview] 로 시작할 수 있으며 #[b http://localhost:3000] 에서 시작합니다.
