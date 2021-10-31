98eda8b0b48e8926f39f136e382b0bb30dc15852
pronist <pronist@users.noreply.github.com>
2021-10-31 10:43:19 +0000

    deploy: f34d588409008f35acccdf21aef69ed6acac8bf3

---
+++ _nuxt/static/1635676998/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdocs\u002Fconfiguration","\u002Fdocs\u002Fexample","\u002Fdocs\u002Fdirectory","\u002Fdocs\u002Fframework","\u002Fdocs\u002Fget-started","\u002Fdocs\u002Fpackage","\u002Fdocs\u002Ftemplate","\u002Fdocs\u002Fdeployment"]})
