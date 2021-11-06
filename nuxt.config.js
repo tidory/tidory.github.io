fed40532a172153f205dddc3e00de5ada489d63c
SangWoo Jeong <pronist@naver.com>
2021-11-06 12:49:27 +0900

    Delete vue-async-computed

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/vue-async-computed'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
