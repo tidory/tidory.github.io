f6beee9ecc88d5efdda51c9312bdfe460213803d
pronist <pronist@naver.com>
2022-09-05 17:56:36 +0900

    Update styling

--- components/app/AppSpy.vue
+++ components/app/AppSpy.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   #spy
-    button(class="fixed right-0 top-28 bg-black/70 w-8 h-8 flex justify-center items-center rounded xl:hidden" @click="show=!show" v-if="!show")
+    button(class="fixed right-0 top-28 bg-gray-500 w-8 h-8 flex justify-center items-center rounded xl:hidden" @click="show=!show" v-if="!show")
       i(class="fas fa-tasks text-gray-50")
-    div(:class="[show ? 'block' : 'hidden']" class="fixed p-4 right-4 top-16 bg-black/90 rounded list-none text-left z-0 xl:w-44 xl:bg-transparent xl:backdrop-blur-0 xl:p-0 xl:absolute xl:top-0 xl:right-auto xl:ml-[calc(620px+110px)] xl:h-full xl:block")
+    div(:class="[show ? 'block' : 'hidden']" class="fixed p-4 right-4 top-16 bg-gray-500 rounded list-none text-left z-0 xl:w-44 xl:bg-transparent xl:backdrop-blur-0 xl:p-0 xl:absolute xl:top-0 xl:right-auto xl:ml-[calc(620px+110px)] xl:h-full xl:block")
       ul(class="xl:sticky xl:top-9 xl:box-border")
         li(v-for="link of toc" :key="link.id" :class="{ 'pl-0': link.depth === 2, 'pl-4': link.depth === 3 }" class="my-2 box-border first:mt-0 last:mb-0")
           a(:href="`#${(link.id)}`" class="text-gray-100 break-all text-sm xl:hover:text-gray-50") {{ link.text }}
