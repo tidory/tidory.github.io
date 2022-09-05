47a259425ebbce9b8743ff7e2d834d4666d148c4
pronist <pronist@users.noreply.github.com>
2022-09-05 08:48:13 +0000

    deploy: ee39d5ade16316497082a5d48e576b7aa5aa78d8

--- _nuxt/static/1662367172/docs/package/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662367172",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fpackage",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"602543a9"}}}}(null));
