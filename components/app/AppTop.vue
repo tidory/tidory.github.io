d0459e3be468c6e9549017b1831d290c757e1765
pronist <pronist@naver.com>
2022-09-05 15:27:19 +0900

    Update AppTop.vue

--- components/app/AppTop.vue
+++ components/app/AppTop.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-  #top(class="hidden xl:block")
-    .notice(class="absolute top-9 left-72 text-sm text-normal")
+  #top(class="w-[calc(100%-260px)] px-5 box-border absolute right-0 top-7 justify-between hidden xl:flex")
+    .notice(class="text-sm text-normal")
       i(class="fas fa-volume-up mr-2.5")
       a.text(
         href="https://pronist.tistory.com/5"
@@ -8,7 +8,7 @@
         rel='noreferrer'
       )
         | hELLO 티스토리 스킨을 소개합니다.
-    ul.menu(class="overflow-hidden list-none absolute top-9 right-9")
+    ul.menu(class="overflow-hidden list-none")
       li(v-for="icon in icons" class="mx-2.5 float-left text-[0] last:mr-0")
         a(:href="icon.href" target='_blank' rel='noreferrer')
           | {{ icon.title }}
