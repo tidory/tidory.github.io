f38bd60f4c6e11dcbe3924ff1c221855f6fc6361
pronist <pronist@naver.com>
2022-12-01 00:07:41 +0900

    Update mobile nav style

--- components/app/AppNav.vue
+++ components/app/AppNav.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
-  #nav(:class="[show ? 'block' : 'hidden']" class="fixed box-border z-30 overflow-hidden bg-gray-300 w-full h-[90%] bottom-0 rounded-t-3xl xl:rounded-none xl:top-0 xl:w-64 xl:h-full xl:block")
-    div(class="overflow-hidden overflow-y-auto w-full h-[90%] xl:overflow-y-scroll xl:h-full xl:w-80 xl:absolute")
-      nuxt-link.logo(to="/" class="text-[0] hidden xl:block xl:pl-8 xl:pt-10 xl:pb-16")
+  #nav(:class="[show ? 'block' : 'hidden']" class="fixed box-border z-30 overflow-hidden bg-gray-300 top-0 w-64 h-full xl:block")
+    div(class="overflow-hidden overflow-y-auto overflow-y-scroll h-full w-80 absolute")
+      nuxt-link.logo(to="/" class="text-[0] block pl-8 pt-10 pb-16")
         | 홈
         img(src="~/assets/images/logo.png" alt="티스토리 로고" width="24" height="24")
-      nav(class="list-none box-border w-full xl:w-56 xl:pl-8 xl:pb-8")
-        ul(v-for="menuItem in menu" class="py-3 px-10 first:pt-8 last:pb-0 xl:px-0 xl:py-4 xl:first:pt-0")
+      nav(class="list-none box-border w-56 pl-8 pb-8")
+        ul(v-for="menuItem in menu" class="py-3 px-10 first:pt-8 last:pb-0 px-0 py-4 first:pt-0")
           li.category(class="font-semibold text-gray-50")
             | {{ menuItem.category }}
-            ul(class="grid grid-cols-2 py-4 xl:block xl:py-0")
-              li.item(v-for="item in menuItem.items" class="xl:first:pt-5")
+            ul(class="block py-0")
+              li.item(v-for="item in menuItem.items" class="first:pt-5")
                 nuxt-link(:to="item.path" v-if="item.path.startsWith('/')")
                   | {{ item.title }}
                 a(:href="item.path" target='_blank' rel='noreferrer' v-else)
@@ -76,7 +76,7 @@ export default {
 <style>
 #nav {
   .item > a {
-    @apply font-normal py-2.5 inline-block xl:py-3;
+    @apply font-normal inline-block py-3;
 
     &.nuxt-link-active {
       @apply text-gray-50 font-medium
