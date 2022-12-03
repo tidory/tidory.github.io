a7b4fc93b21ceba9e21b88a8c7f66139b16db107
pronist <pronist@naver.com>
2022-12-04 00:52:45 +0900

    Add scrollspy

--- components/app/AppSpy.vue
+++ components/app/AppSpy.vue
@@ -3,7 +3,7 @@
     button(class="fixed right-0 top-28 bg-gray-500 w-8 h-8 flex justify-center items-center rounded xl:hidden" @click="show=!show" v-if="!show")
       i(class="fas fa-tasks text-gray-50")
     div(:class="[show ? 'right-4' : '-right-52']" class="fixed p-4 right-4 w-48 top-16 bg-gray-500 rounded list-none text-left z-0 transition-all xl:w-40 xl:bg-transparent xl:backdrop-blur-0 xl:p-0 xl:absolute xl:top-0 xl:right-auto xl:ml-[calc(620px+110px)] xl:h-full xl:block xl:transition-none")
-      ul(class="xl:sticky xl:top-9 xl:box-border")
+      ul(class="xl:sticky xl:top-9 xl:box-border" ref="nav")
         li(v-for="link of toc" :key="link.id" :class="{ 'pl-0': link.depth === 2, 'pl-4': link.depth === 3 }" class="my-2 box-border first:mt-0 last:mb-0")
           a(:href="`#${(link.id)}`" class="text-gray-100 break-all text-sm xl:hover:text-gray-50") {{ link.text }}
 </template>
@@ -15,11 +15,77 @@ export default {
   },
   data () {
     return {
-      show: false
+      show: false,
+      className: 'active',
+      offset: 128
+    }
+  },
+  computed: {
+    headings () {
+      return this.content.querySelectorAll('h2, h3')
+    },
+    content () {
+      return document.querySelector('#document > article')
     }
   },
   created () {
     this.$nuxt.$on('toc', () => { this.show = false })
+  },
+  mounted () {
+    window.addEventListener('scroll', () => this.spy(this.headings))
+  },
+  methods: {
+    /**
+     * @param {HTMLCollection} $headings
+     */
+    spy ($headings) {
+      for (const $heading of $headings) {
+        if (this.in($heading)) {
+          this.init()
+          this.active($heading)
+        }
+      }
+
+      if (this.out($headings.item(0))) {
+        this.init()
+      }
+    },
+    init () {
+      const $links = this.$refs.nav.querySelectorAll('li > a')
+
+      for (const $link of $links) {
+        $link.classList.remove(this.className)
+      }
+    },
+    /**
+     * @param {HTMLElement} $heading
+     */
+    active ($heading) {
+      const $menuItem = this.$refs.nav.querySelector(`a[href="#${$heading.id}"]`)
+      $menuItem.classList.add(this.className)
+    },
+    /**
+     * @param {HTMLElement} $heading
+     * @returns {boolean}
+     */
+    in ($heading) {
+      return $heading.offsetTop + this.offset < window.scrollY
+    },
+    /**
+     * @param {HTMLElement} $heading
+     * @returns {boolean}
+     */
+    out ($heading) {
+      return $heading.offsetTop + this.offset > window.scrollY
+    }
   }
 }
 </script>
+
+<style>
+#spy {
+  a.active {
+    @apply text-gray-50 !important
+  }
+}
+</style>
